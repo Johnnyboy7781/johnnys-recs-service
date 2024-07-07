d2cfc193fd2bc9a911176ac5dc7791caa662e8eb
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2024-07-07 18:52:16 -0400

    update app listen call

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ if (process.env.NODE_ENV !== 'production') {
 // call after config() to access the env variables
 import { app } from './api';
 
-const port = process.env.PORT || 3000;
+const port: number = Number(process.env.PORT) || 3000;
 
-app.listen(port, () =>
-  console.log(`API available on port: ${port}`)
+app.listen(port, "::", () =>
+  console.log(`API available on port [::]${port}`)
 );
