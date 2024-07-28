ece151bd81d6b4693649c4e0afe7c3d7f04f2573
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2024-07-28 16:39:36 -0400

    remove unused endpoint

--- src/routes/subregion.ts
+++ src/routes/subregion.ts
@@ -4,12 +4,6 @@ import { prisma } from "../utils/prisma-entry";
 
 export const subregionRouter = express.Router();
 
-// Get all subregions
-subregionRouter.get('/', async (req, res) => {
-    const result = await prisma.tbl_sub_region.findMany();
-    res.json(result);
-});
-
 // Get subregions by region id
 subregionRouter.get('/region/:id', async (req, res) => {
     const id = parseInt(req.params.id);
